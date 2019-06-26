956bfa1972e209374ff6bd0ffac2181794cdd295
Alex Kite <alexkite511@gmail.com>
2019-06-25 21:18:22 -0700

    first commit

---
+++ src/AllNots.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+import Notification from './Notification';
+import InvitationsJson from './invitations';
+import NewInvitationsJson from './invitations_update';
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40%;
+  padding: 0;
+  margin: 0;
+  background-color: white;
+  padding-bottom: 8px;
+`
+
+const convertToInviteObj = json => {
+  let arr = json.invites;
+  let obj = {};
+  arr.forEach(invite => { obj[invite.invite_id] = invite });
+  return obj;
+}
+
+const ShowUnreadToggle = styled.button`
+  cursor: pointer;
+  border: 1px solid grey;
+  margin-top: 5px;
+`
+
+class AllNots extends React.Component {
+  constructor(props) {
+    super(props);
+    this.isInititialRender = true;
+    this.state = { 
+      showread: true,
+      notifications: convertToInviteObj(InvitationsJson) 
+    };
+  }
+
+  componentDidMount = () => {
+    setTimeout(() => { this.addNewNotifications(NewInvitationsJson) }, 3000);
+  }
+
+  addNewNotifications = (newNotications) => {
+    let newNoticationsObj = convertToInviteObj(newNotications);
+    this.setState(prevState => {
+      let merged = Object.assign({}, prevState.notifications, newNoticationsObj);
+      return { notifications: merged };
+    })
+  }
+
+  toggleNotiStatusHelper = (inviteId, prevState) => {
+    let notification = Object.assign({}, prevState.notifications[inviteId]);
+    notification.status = notification.status === 'read'? 'unread' : 'read';
+    let newNotis = Object.assign({}, prevState.notifications, {[inviteId]: notification});
+    let newState = {notifications: newNotis}
+    return newState;
+  }
+
+  toggleNotiStatus = (inviteId) => {
+    this.setState(prevState => this.toggleNotiStatusHelper(inviteId, prevState));
+  }
+
+  toggleShowRead = () => {
+    this.setState(prevState => ({showread: !prevState.showread}))
+  }
+
+
+
+  render() {
+    let notiArray = Object.keys(this.state.notifications).map( key => this.state.notifications[key]);
+    if (this.state.showread === false) {
+      notiArray = notiArray.filter( noti => noti.status === 'unread');
+    }
+    let notiList = notiArray.map((noti) => {
+      return <Notification toggleNoti={this.toggleNotiStatus} key={noti.invite_id} noti={noti}></Notification>
+    });
+    
+    return (
+      <Main>
+        <ShowUnreadToggle onClick={this.toggleShowRead}>{this.state.showread? 'Hide Read' : 'Show Read'}</ShowUnreadToggle>
+        {
+          notiList
+        }
+      </Main>
+    );
+  }
+
+}
+
+export default AllNots;
---
+++ src/Notification.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import styled from 'styled-components';
+import EmailIcon from './email-icon.png';
+import CrestLogo from './crest-logo.png';
+
+const Main = styled.li`
+    width: 90%;
+    height: 55px;
+    margin-top: 8px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    flex-shrink: 0;
+    box-shadow: 5px 20px 30px rgba(0, 0, 0, 0.25);
+    font-size: 10px;
+    cursor: pointer;
+`
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-grow: 1;
+    background-color: ${props => props.status === 'read' ? '#CBD3DC' : '#C6EEF0'};
+    padding-left: 7px;
+    padding-right: 7px;
+`
+
+const Message = styled.div`
+    flex-grow: 2;
+    display: flex;
+    align-items: center;
+`
+
+const Sender = styled.div`
+    margin-right: 6px;
+`
+
+const VectorContainer = styled.div`
+    margin-right: 7px;
+    height: auto;
+    width: 15px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    border-radius: 2px;
+`
+
+const VectorImage = styled.img`
+    width: 100%;
+`
+
+const HeaderPart = styled.div`
+    display: flex;
+    align-items: center;
+    height: 100%;
+`
+
+const Status = styled.div`
+
+`
+
+const timeIntToString = (time) => {
+    let newTime = new Date(time);
+    return newTime.toLocaleDateString() + ' ' + newTime.toLocaleTimeString()
+}
+
+const getOppositeButtonText = (current) => {
+    let result = current === 'read'? 'Mark Unread': 'Mark Read';
+    return result;
+}
+
+class Notification extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showReadButton: false
+        }
+    }
+
+    toggleReadButton = () => {
+
+        this.setState( prevState => ({showReadButton: !prevState.showReadButton}));
+    }
+    
+    render() {
+        let notiInfo = this.props.noti;
+    
+        return (
+            
+            <Main onClick={() => this.props.toggleNoti(notiInfo.invite_id)}>
+                <Header onMouseEnter={this.toggleReadButton} status={notiInfo.status}>
+                    <HeaderPart>
+                        <VectorContainer>
+                            <VectorImage src={notiInfo.vector === 'Email' ? EmailIcon : CrestLogo} />
+                        </VectorContainer>
+                        <Sender>{notiInfo.sender_id}</Sender>
+                        {timeIntToString(notiInfo.invite_time)}
+                    </HeaderPart>
+                    <HeaderPart>
+                        <Status >
+                            {
+                                notiInfo.status
+                            }
+                        </Status>
+                    </HeaderPart>
+                </Header>
+                <Message>
+                    {notiInfo.invite}
+                </Message>
+            </Main>
+        );
+    }
+}
+
+export default Notification;
